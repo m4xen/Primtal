a3caffe82b9c3cd83ef3c33c3f6b77f69d3f0884
Max Runesson <Max@maxRunesson.se>
2021-01-13 13:09:48 +0100

    version 1

--- primetalExercise/Program.cs
+++ primetalExercise/Program.cs
@@ -10,6 +10,34 @@ namespace primetalExercise
     {
         static void Main(string[] args)
         {
+            int inputNum = 1;
+
+            if (IsPrime(inputNum))
+            {
+                Console.WriteLine(inputNum + " is a prime number");
+            }
+            else
+            {
+                Console.WriteLine(inputNum + " is not a prime number");
+            }
+            
+            Console.WriteLine("\n\n Press any key to exit \n");
+            Console.ReadKey();
+        }
+
+        private static bool IsPrime(int inputNum)
+        {
+            if (inputNum <= 1) return false;
+            if (inputNum == 2) return true;
+            if (inputNum % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(inputNum));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (inputNum % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
